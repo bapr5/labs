3b72c49950b950dcbefd5b7dc5f943ba7fe9649b
pr5 <pr5@gnome.su>
2023-09-14 11:55:43 +0300

    fixed 2nd lab

--- py/2.py
+++ py/2.py
@@ -7,27 +7,41 @@ Created on Thu Sep  7 22:56:34 2023
 """
 
 graph = {
-'1':[]
-    }
+    '1': ["2", "3"],
+    '2': ["4", "5", "1"],
+    '3': ["6", "1"],
+    '4': ["7", "2"],
+    '5': ["8", "6"],
+    '6': ["5", "3", "9"],
+    '7': ["4"],
+    '8': ["5"],
+    '9': ["6"]
+
+}
 
 visited = []
 queue = []
 
 
-def bfs(visited:list, graph:dict, entry_node:str):  
+def bfs(visited: list, graph: dict, query: str, entry_node='1'):
     visited.append(entry_node)
     queue.append(entry_node)
 
     while queue:
         m = queue.pop(0)
+        print(f"Currently in node {m}")
+        if(m==query):
+            print("FOUND")
+            break
+        for neighbour_node in graph[m]:
+            if neighbour_node not in visited:
+                visited.append(neighbour_node)
+                queue.append(neighbour_node)
 
 
-        for neighbour in graph[m]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
-
-
-
-
-bfs(visited, graph, )   
+e = input("Enter entry point to graph: ")
+q = input("Enter graph node to find: ")
+try:
+    bfs(visited, graph, q, e)
+except err as e:
+    print("no such point on graph or invalid query")
