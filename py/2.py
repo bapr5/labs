22b3ec48be61ce49d6378d6de1b2f89b8701dede
pr5 <pr5@gnome.su>
2023-09-14 09:57:20 +0300

    bfs algo + pic

--- py/2.py
+++ py/2.py
@@ -6,3 +6,28 @@ Created on Thu Sep  7 22:56:34 2023
 @author: pr5
 """
 
+graph = {
+'1':[]
+    }
+
+visited = []
+queue = []
+
+
+def bfs(visited:list, graph:dict, entry_node:str):  
+    visited.append(entry_node)
+    queue.append(entry_node)
+
+    while queue:
+        m = queue.pop(0)
+
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+
+
+bfs(visited, graph, )   
